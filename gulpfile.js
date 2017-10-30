db4ce1fdd7b253e3d0a04ab9d6d88f2a35e49b9f
Igor Levchenko <igorvk7@gmail.com>
2017-10-30 23:24:10 +0200

    added project config files & remove load folders

--- gulpfile.js
+++ gulpfile.js
@@ -16,6 +16,11 @@ const plumber = require('gulp-plumber');
 const notify = require('gulp-notify');
 const gcmq = require('gulp-group-css-media-queries');
 
+// Project config
+let projectConfig = require('./projectConfig.json');
+let srcPath = projectConfig.path.src || './src/';
+let distPath = projectConfig.path.dist || './build/';
+
 // Settings
 let postCssSettings = [
   autoprefixer({browsers: ['last 2 version']})
@@ -25,80 +30,80 @@ let postCssSettings = [
 
 // Style
 gulp.task('style', function() {
- return gulp.src('./src/sass/main.scss')
+ return gulp.src(srcPath + 'sass/main.scss')
   .pipe(sourcemaps.init())
   .pipe(sass().on('error', sass.logError))
   .pipe(postcss(postCssSettings))
   .pipe(sourcemaps.write('/'))
-  .pipe(gulp.dest('./build/styles'))
+  .pipe(gulp.dest(distPath + 'styles'))
   .pipe(browserSync.stream({match: '**/*.css'}));
 });
 
 
 // Style build
 gulp.task('style-build', function() {
- return gulp.src('./src/sass/main.scss')
+ return gulp.src(srcPath + 'sass/main.scss')
   .pipe(sass().on('error', sass.logError))
   .pipe(gcmq())
   .pipe(postcss(postCssSettings))
-  .pipe(gulp.dest('./build/styles'));
+  .pipe(gulp.dest(distPath + 'styles'));
 });
 
 
 // Copy files
 gulp.task('copy:scripts', function() {
-  return gulp.src('./src/scripts/*.{js,json}')
-    .pipe(newer('./build/scripts'))
-    .pipe(gulp.dest('./build/scripts'));
+  return gulp.src(srcPath + 'scripts/*.{js,json}')
+    .pipe(newer(distPath + 'scripts'))
+    .pipe(gulp.dest(distPath + 'scripts'));
 });
 
 gulp.task('copy:images', function() {
-  return gulp.src('./src/images/**/*.{jpg,jpeg,png,gif,svg}')
-    .pipe(newer('./build/images'))
-    .pipe(gulp.dest('./build/images'));
+  return gulp.src(srcPath + 'images/**/*.{jpg,jpeg,png,gif,svg}')
+    .pipe(newer(distPath + 'images'))
+    .pipe(gulp.dest(distPath + 'images'));
 });
 
 gulp.task('copy:fonts', function() {
-  return gulp.src('./src/fonts/**/*.{ttf,woff,woff2,eot,svg}')
-    .pipe(newer('./build/fonts'))
-    .pipe(gulp.dest('./build/fonts'));
+  return gulp.src(srcPath + 'fonts/**/*.{ttf,woff,woff2,eot,svg}')
+    .pipe(newer(distPath + 'fonts'))
+    .pipe(gulp.dest(distPath + 'fonts'));
 });
 
 gulp.task('copy:php', function() {
-  return gulp.src('./src/**/*.php')
-    .pipe(newer('./build'))
-    .pipe(gulp.dest('./build'));
+  return gulp.src(srcPath + '**/*.php')
+    .pipe(newer(distPath))
+    .pipe(gulp.dest(distPath));
 });
 
 
 // Concatenate scripts and styles
 gulp.task('concat:scripts', function() {
-  return gulp.src('./src/_load-scripts/*.js')
+  return gulp.src(projectConfig.assets.addJs)
     .pipe(concat('load-scripts.js'))
-    .pipe(gulp.dest('./build/scripts'));
+    .pipe(gulp.dest(distPath + 'scripts'));
 });
 
 gulp.task('concat:styles', function() {
-  return gulp.src('./src/_load-styles/*.css')
+  return gulp.src(projectConfig.assets.addCss)
     .pipe(concat('load-styles.css'))
-    .pipe(gulp.dest('./build/styles'));
+    .pipe(gulp.dest(distPath + 'styles'));
 });
 
 
 // Minify styles and scripts
 gulp.task('minify:css', function() {
   const cleanCSS = require('gulp-clean-css');
-  return gulp.src('./build/styles/*.css')
+  return gulp.src(distPath + 'styles/*.css')
     .pipe(cleanCSS())
     .pipe(rename(function (path) {
       path.extname = '.min.css'
     }))
-    .pipe(gulp.dest('./build/styles'));
+    .pipe(gulp.dest(distPath + 'styles'));
 });
 
 gulp.task('minify:scripts', function() {
   const uglify = require('gulp-uglify');
-  return gulp.src('./build/scripts/*.js')
+  return gulp.src(distPath + 'scripts/*.js')
     .pipe(plumber({
       errorHandler: function(err) {
         notify.onError({
@@ -114,19 +119,19 @@ gulp.task('minify:scripts', function() {
     .pipe(rename(function (path) {
       path.extname = '.min.js'
     }))
-    .pipe(gulp.dest('./build/scripts'));
+    .pipe(gulp.dest(distPath + 'scripts'));
 });
 
 
 // optimize images
 gulp.task('optimize-images', function() {
   const imagemin = require('gulp-imagemin');
-  return gulp.src('./build/images/**/*.{jpg,jpeg,gif,png,svg}')
+  return gulp.src(distPath + 'images/**/*.{jpg,jpeg,gif,png,svg}')
     .pipe(imagemin({
       progressive: true,
       svgoPlugins: [{removeViewBox: false}]
     }))
-    .pipe(gulp.dest('./build/images'));
+    .pipe(gulp.dest(distPath + 'images'));
 });
 
 
@@ -134,7 +139,7 @@ gulp.task('optimize-images', function() {
 gulp.task('create-sprite', function() {
   const spritesmith = require('gulp.spritesmith');
   const merge = require('merge-stream');
-  let spriteData = gulp.src('./src/images/sprite/*.png').pipe(spritesmith({
+  let spriteData = gulp.src(srcPath + 'images/sprite/*.png').pipe(spritesmith({
     imgName: 'spritesheet.png',
     cssName: '_sprite.scss',
     padding: 2,
@@ -142,9 +147,9 @@ gulp.task('create-sprite', function() {
     imgPath: '../images/spritesheet.png'
   }));
   let imgStream = spriteData.img
-    .pipe(gulp.dest('./src/images/'));
+    .pipe(gulp.dest(srcPath + 'images/'));
   let cssStream = spriteData.css
-    .pipe(gulp.dest('./src/sass/'));
+    .pipe(gulp.dest(srcPath + 'sass/'));
   return merge(imgStream, cssStream);
 });
 
@@ -152,7 +157,7 @@ gulp.task('create-sprite', function() {
 // Include HTML
 gulp.task('html', function() {
   const fileinclude = require('gulp-file-include');
-  return gulp.src('./src/*.html')
+  return gulp.src(srcPath + '*.html')
     .pipe(plumber({
       errorHandler: function(err) {
         notify.onError({
@@ -167,7 +172,7 @@ gulp.task('html', function() {
       basepath: '@file',
       indent: true,
     }))
-    .pipe(gulp.dest('./build'));
+    .pipe(gulp.dest(distPath));
 });
 
 
@@ -175,11 +180,11 @@ gulp.task('html', function() {
 gulp.task('clean', function() {
   const del = require('del');
   return del([
-    './build/*.html',
-    './build/fonts',
-    './build/images',
-    './build/scripts',
-    './build/styles'
+    distPath + '*.html',
+    distPath + 'fonts',
+    distPath + 'images',
+    distPath + 'scripts',
+    distPath + 'styles'
   ]);
 });
 
@@ -188,12 +193,12 @@ gulp.task('clean', function() {
 
 gulp.task('replace-path', function() {
   const replace = require('gulp-replace');
-  gulp.src(['./build/*.html'])
+  gulp.src([distPath + '*.html'])
     .pipe(replace('load-styles.css', 'load-styles.min.css'))
     .pipe(replace('main.css', 'main.min.css'))
     .pipe(replace('load-scripts.js', 'load-scripts.min.js'))
     .pipe(replace('main.js', 'main.min.js'))
-    .pipe(gulp.dest('./build'));
+    .pipe(gulp.dest(distPath));
 });
 
 // Main tasks
@@ -232,30 +237,26 @@ gulp.task('sprite', function(callback) {
 // Watch
 gulp.task('serve', ['build-dev'], function() {
   browserSync.init({
-    server: './build/',
+    server: distPath,
     startPath: '/',
     open: false,
     port: 5050,
   });
-  gulp.watch('src/sass/**/*.scss', {cwd: './'}, ['style']);
-  gulp.watch('src/scripts/*.{js,json}', {cwd: './'}, ['watch:scripts']);
-  gulp.watch('src/images/**/*.{jpg,jpeg,gif,png,svg}', {cwd: './'}, ['watch:images']);
-  gulp.watch('src/fonts/**/*.{ttf,woff,woff2,eot,svg}', {cwd: './'}, ['watch:fonts']);
-  gulp.watch('src/**/*.php', {cwd: './'}, ['watch:php']);
-  gulp.watch('src/_load-scripts/*.js', {cwd: './'}, ['watch:concat-scripts']);
-  gulp.watch('src/_load-styles/*.css', {cwd: './'}, ['watch:concat-styles']);
+  gulp.watch('sass/**/*.scss', {cwd: srcPath}, ['style']);
+  gulp.watch('scripts/*.{js,json}', {cwd: srcPath}, ['watch:scripts']);
+  gulp.watch('images/**/*.{jpg,jpeg,gif,png,svg}', {cwd: srcPath}, ['watch:images']);
+  gulp.watch('fonts/**/*.{ttf,woff,woff2,eot,svg}', {cwd: srcPath}, ['watch:fonts']);
+  gulp.watch('**/*.php', {cwd: srcPath}, ['watch:php']);
   gulp.watch([
-    'src/*.html',
-    'src/_include/**/*.html'
-  ], {cwd: './'}, ['watch:html']);
+    '*.html',
+    '_include/**/*.html'
+  ], {cwd: srcPath}, ['watch:html']);
 });
 
 gulp.task('watch:scripts', ['copy:scripts'], reload);
 gulp.task('watch:images', ['copy:images'], reload);
 gulp.task('watch:fonts', ['copy:fonts'], reload);
 gulp.task('watch:php', ['copy:php'], reload);
-gulp.task('watch:concat-scripts', ['concat:scripts'], reload);
-gulp.task('watch:concat-styles', ['concat:styles'], reload);
 gulp.task('watch:html', ['html'], reload);
 
 
